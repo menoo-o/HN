45ee218b1b70b68f171f653ef7ae805e6e8be487
menoo-o <menosuper6@gmail.com>
2024-10-29 14:42:21 +0500

    landing pg in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,58 @@
 import Image from 'next/image'
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faYoutube,
+  faSpotify,
+  faSoundcloud,
+  faFacebook,
+  faInstagram,
+  faTwitter
+} from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
   return (
     <>
-      <div className='hero'>
-        <Image 
+
+     <div className='hero-section'>
+      <Image
+        className="logo-img"
+        src="/logo/4.jpg"
+        alt="Logo img"
+        width={260}
+        height={230}
+      />
+      <Image
+        className="hero-img"
         src="/hero2.jpg"
-        alt='hero img'
-        width={470}
-        height={850}
-        />
+        alt="Hero img"
+        width={435}
+        height={835}
+      />
+    </div>
+    <div className='icons-left'>
+        <FontAwesomeIcon icon={faYoutube} /> 
+        <FontAwesomeIcon icon={faSpotify} /> 
+        <FontAwesomeIcon icon={faSoundcloud} /> 
       </div>
+      <div className='icons-right'>
+        <FontAwesomeIcon icon={faFacebook} /> 
+        <FontAwesomeIcon icon={faInstagram} /> 
+        <FontAwesomeIcon icon={faTwitter} /> 
+      </div>
+
+    <footer className='footer'>
+      <nav>
+        <ul className='pg-links'>
+          <li><Link href='/'>HOME</Link></li>
+          <li><Link href='/music'>MUSIC</Link></li>
+          <li><Link href='/shows'>SHOWS</Link></li>
+          <li><Link href='/contact'>CONTACT</Link></li>
+        </ul>
+     </nav>
+</footer>
+
+
 
     </>
   );
